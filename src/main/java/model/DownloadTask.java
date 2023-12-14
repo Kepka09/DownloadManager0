e28eb06c2971b3d9e033982cb2fa4978212cb9eb
Kepka09 <kepinphilosopher@gmail.com>
2023-12-14 17:42:51 +0300

    lab4 Iterator

---
+++ src/main/java/model/DownloadTask.java
@@ -0,0 +1,40 @@
+package model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+
+    public class DownloadTask {
+
+    private String fileName;
+    private String url;
+    private int downloadSpeed; // в килобайтах в секунду
+    private boolean isInProgress;
+
+    public void start() {
+        isInProgress = true;
+        //Todo: тут надо будет сделать реальную загрузку файла по параметрам
+    }
+
+    public void pause() {
+        isInProgress = false;
+        //todo: тут надо будет остановить загрузку файла
+    }
+
+    public void delete() {
+        isInProgress = false;
+        //todo: тут надо будет удалить загрузку
+    }
+
+
+
+
+}
