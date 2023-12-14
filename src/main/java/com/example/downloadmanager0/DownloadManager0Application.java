a6b7b8f8488025d0590e88e7cba6126c70cce73f
Kepka09 <kepinphilosopher@gmail.com>
2023-12-14 17:23:46 +0300

    Iterator pattern

---
+++ src/main/java/com/example/downloadmanager0/DownloadManager0Application.java
@@ -0,0 +1,13 @@
+package com.example.downloadmanager0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadManager0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadManager0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/downloadmanager0/DownloadManager0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downloadmanager0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadManager0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
